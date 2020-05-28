34a8901d870e981fa397d35e898614d290725726
Yuliia Konovalenko <rastishka29@gmail.com>
2020-05-27 22:03:01 -0700

    Coding Exercise 28: Paint Job

--- ReadingUserInput/src/Main.java
+++ ReadingUserInput/src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        ReadingUserInput.readingInput();
-        InputChallenge.inputThenPrintSumAndAverage();
+        // Reading Users Input Challenge
+
+//        ReadingUserInput.readingInput();
+//        InputChallenge.inputThenPrintSumAndAverage();
+
+        // Paint Job Challenge
+        System.out.println(PaintJob.getBucketCount(-3.4, 2.1, 1.5, 2));
+        System.out.println(PaintJob.getBucketCount(3.4, 2.1, 1.5, 2));
+
+        System.out.println(PaintJob.getBucketCount(-3.4, 2.1, 1.5));
+        System.out.println(PaintJob.getBucketCount(3.4, 2.1, 1.5));
+        System.out.println(PaintJob.getBucketCount(7.25, 4.3, 2.35));
+
+        System.out.println(PaintJob.getBucketCount(3.4, 1.5));
+        System.out.println(PaintJob.getBucketCount(6.26, 2.2));
+        System.out.println(PaintJob.getBucketCount(3.26, 0.75));
+
     }
 }
