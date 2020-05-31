bab54530fdc3381c3a7d6ff258e2f40ffa779a8f
Yuliia Konovalenko <rastishka29@gmail.com>
2020-05-30 17:04:29 -0700

    Section 6. OOP, Classes. Coding Exercise 29 SimpleCalculator Coding Exercise 30. Person

--- Section 6_OOP/src/rastishka/com/Main.java
+++ Section 6_OOP/src/rastishka/com/Main.java
@@ -3,13 +3,42 @@ package rastishka.com;
 public class Main {
 
     public static void main(String[] args) {
-	Car porsche = new Car();
-	Car holden = new Car();
 
-	// System.out.println("Model is " + porsche.getModel()); // Model is null
-		//porsche.setModel("911"); // Model is Unknown;
+    	// Car example
+        Car porsche = new Car();
+        Car holden = new Car();
 
-	porsche.setModel("Carrera");
-	System.out.println("Model is " + porsche.getModel()); // Model is Carrera
+        // System.out.println("Model is " + porsche.getModel()); // Model is null
+        //porsche.setModel("911"); // Model is Unknown;
+
+        porsche.setModel("Carrera");
+        System.out.println("Model is " + porsche.getModel()); // Model is Carrera
+
+		// Coding Exercise 30. Simple Calculator challenge.
+
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
+
+        // Coding Exercise 31. Person challenge.
+
+		Person person = new Person();
+		person.setFirstName("");
+		person.setLastName("");
+		person.setAge(10);
+		System.out.println("fullName= " + person.getFullName());
+		System.out.println("teen= " + person.isTeen());
+		person.setFirstName("John");
+		person.setAge(18);
+		System.out.println("fullName= " + person.getFullName());
+		System.out.println("teen= " + person.isTeen());
+		person.setLastName("Smith");
+		System.out.println("fullName= " + person.getFullName());
     }
 }
---
+++ Section 6_OOP/src/rastishka/com/Person.java
@@ -0,0 +1,48 @@
+package rastishka.com;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public void setAge(int age){
+        if(age < 0 || age > 100){
+            this.age = 0;
+        } else{
+            this.age = age;
+        }
+    }
+    public boolean isTeen(){
+        return age < 20 && age > 12;
+    }
+    public String getFullName(){
+        if(firstName.isEmpty() && lastName.isEmpty()){
+            return "";
+        } else if (firstName.isEmpty()){
+            return lastName;
+        } else if (lastName.isEmpty()){
+            return firstName;
+        } else {
+            return firstName + " " + lastName;
+        }
+    }
+}
