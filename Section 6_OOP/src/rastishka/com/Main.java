06e7850296cc4862e92a17f6d785c7fb710c93d7
Yuliia Konovalenko <rastishka29@gmail.com>
2020-06-03 22:49:44 -0700

    Coding Exercise 33. Carpet Cost Calculator

--- Section 6_OOP/src/rastishka/com/Main.java
+++ Section 6_OOP/src/rastishka/com/Main.java
@@ -107,6 +107,21 @@ public class Main {
 		Point point = new Point();
 		System.out.println("distance(0,0)= " + point.distance());
 
+		// Coding Exercise 33. Carpet Cost Challenge.
+
+		Carpet carpet = new Carpet(3.5);
+		Floor floor = new Floor(2.75, 4.0);
+		Calculator calc = new Calculator(floor, carpet);
+		System.out.println("total= " + calc.getTotalCost());
+
+		carpet = new Carpet(1.5);
+		floor = new Floor(5.4, 4.5);
+		calc = new Calculator(floor, carpet);
+		System.out.println("total= " + calc.getTotalCost());
+
+
+
+
 
 
 	}
