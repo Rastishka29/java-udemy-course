7803268abb5553e844d16e0e36290e25c48efb5e
Yuliia Konovalenko <rastishka29@gmail.com>
2020-06-09 22:16:24 -0700

    Updated project folder structure.

---
+++ Section 6/Inheritance/src/rastishka/com/Main.java
@@ -0,0 +1,18 @@
+package rastishka.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Animal animal = new Animal("Animal", 1,1,5,5);
+        Dog dog = new Dog("Yorkie", 8,20, 2,4,1,20, "long silky");
+        dog.eat();
+
+        dog.walk();
+        dog.run();
+
+        Fish fish = new Fish("Igor",3,2,2,2, 3);
+        fish.swim(5);
+
+    }
+}
